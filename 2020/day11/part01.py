550eeba9fcb9632499f0ccad47aad4230719a230
Steve Boyle <sysboy@gmail.com>
2020-12-11 15:09:37 +0000

    stupid graphics

--- 2020/day11/part01.py
+++ 2020/day11/part01.py
@@ -1,90 +1,72 @@
 #!/usr/bin/env python3.7
 """ AOC day 11 """
 
+import sys
+
 grid = []
-with open('input.txt') as fd:
+with open(sys.argv[1]) as fd:
     for line in fd:
         # seat and change
-        grid.append([[char,'.'] for char in line[:-1]])
+        grid.append([[char, "."] for char in line[:-1]])
 
 
-ROWLEN=len(grid)
-COLLEN=len(grid[0])
+ROWLEN = len(grid)
+COLLEN = len(grid[0])
 
-def occupation(g,row,col):
+
+def occupation(g, row, col):
     neighbours = 0
     # top
     if row != 0:
-#        print("Testing top")
-        if g[row-1][col][0] == '#':
-#            print("top")
+        if g[row - 1][col][0] == "#":
             neighbours += 1
     # bottom
-    if row != ROWLEN-1:
-#        print(f'[{row}][{col}] Testing bottom')
-        if g[row+1][col][0] == '#':
-#            print("   bottom")
+    if row != ROWLEN - 1:
+        if g[row + 1][col][0] == "#":
             neighbours += 1
     # left
     if col != 0:
-#        print(f'[{row}][{col}] Testing left')
-        if g[row][col-1][0] == '#':
-#            print("  left")
+        if g[row][col - 1][0] == "#":
             neighbours += 1
     # right
-    if col != COLLEN-1:
-#        print(f'[{row}][{col}] Testing right')
-        if g[row][col+1][0] == '#':
-#            print("  right")
+    if col != COLLEN - 1:
+        if g[row][col + 1][0] == "#":
             neighbours += 1
     # topleft
-    if row !=0 and col !=0:
-        if g[row-1][col-1][0] == '#':
-#            print("topleft")
+    if row != 0 and col != 0:
+        if g[row - 1][col - 1][0] == "#":
             neighbours += 1
     # topright
-    if row !=0 and col != COLLEN-1:
-        if g[row-1][col+1][0] == '#':
-#            print("topright")
+    if row != 0 and col != COLLEN - 1:
+        if g[row - 1][col + 1][0] == "#":
             neighbours += 1
     # bottomleft
-    if row != ROWLEN-1 and col !=0:
-#        print(f'[{row}][{col}] Testing bottom left')
-        if g[row+1][col-1][0] == '#':
-#            print("   bottomleft")
+    if row != ROWLEN - 1 and col != 0:
+        if g[row + 1][col - 1][0] == "#":
             neighbours += 1
     # bottomright
-    if row != ROWLEN-1 and col != COLLEN-1:
-#        print(f'[{row}][{col}] Testing bottom right')
-        if g[row+1][col+1][0] == '#':
-#            print("  bottomright")
+    if row != ROWLEN - 1 and col != COLLEN - 1:
+        if g[row + 1][col + 1][0] == "#":
             neighbours += 1
-#    print(f'Score for [{row}][{col}] = {neighbours}')
-    if row == 0 and col == 0:
-        print(f'Score for [{row}][{col}] = {neighbours}')
+
     return neighbours
 
 
 def update(g):
-#    print("Starting update")
     for row in range(ROWLEN):
-#        print('-----------')
         for col in range(COLLEN):
-#            print(f'Testing [{row},{col}][{g[row][col][0]}]')
             seat = g[row][col][0]
             # floor is never occupied
-            if seat == '.':
+            if seat == ".":
                 g[row][col][1] == seat
                 continue
 
-            o = occupation(g,row,col)
-#            print(f'score for [{row},{col}] = {o}')
+            o = occupation(g, row, col)
             if o == 0:
-#                print(f'occupying [{row},{col}]')
-                g[row][col][1] = '#'
-            if o >= 4 and g[row][col][0] == '#':
-#                print(f'vacating [{row},{col}]')
-                g[row][col][1] = 'L'
+                g[row][col][1] = "#"
+            if o >= 4 and g[row][col][0] == "#":
+                g[row][col][1] = "L"
+
 
 def check(g):
     changes = 0
@@ -93,32 +75,26 @@ def check(g):
             if g[row][col][0] != g[row][col][1]:
                 changes = changes + 1
                 g[row][col][0] = g[row][col][1]
-    return g,changes
+    return changes
+
 
 def show(g):
     s = 0
     for row in range(ROWLEN):
         print("".join([s[0] for s in g[row]]))
-#        print("".join([s[0] for s in g[row]]), " ---> ", "".join([s[1] for s in g[row]]))
-        s += "".join([s[0] for s in g[row]]).count('#')
-    print(f'{s} occupied seats')
+        s += "".join([s[0] for s in g[row]]).count("#")
+    print(f"{s} occupied seats")
 
 
 show(grid)
 runs = 1
 while True:
-    print("Run", runs)
     update(grid)
     runs = runs + 1
-#    show(grid)
-    grid,changes = check(grid)
-    print(f'Changes = {changes}')
+    show(grid)
+    changes = check(grid)
     if changes == 0:
         break
 
-
 show(grid)
-print(f'{runs} runs.')
-    
-
-
+print(f"{runs} runs.")
--- 2020/day11/part02.py
+++ 2020/day11/part02.py
@@ -2,135 +2,102 @@
 """ AOC day 11 """
 
 import sys
+from tkinter import *
+import time
+
+
 grid = []
 with open(sys.argv[1]) as fd:
     for line in fd:
         # seat and change
-        grid.append([[char,'.'] for char in line[:-1]])
+        grid.append([[char, "."] for char in line[:-1]])
 
 
-ROWLEN=len(grid)
-COLLEN=len(grid[0])
+ROWLEN = len(grid)
+COLLEN = len(grid[0])
 
-def occupation(g,row,col):
+
+def occupation(g, row, col):
     neighbours = 0
-    testr = -99
-    testc = -99
+
     # top
-    if row == testr and col == testc:
-        print(f'[{row}][{col}] Testing top')
-    for r in range(row-1,-1,-1):
-        if g[r][col][0] == 'L':
+
+    for r in range(row - 1, -1, -1):
+        if g[r][col][0] == "L":
             break
-        if g[r][col][0] == '#':
-            if row == testr and col == testc:
-                print(f'   occupied at [{r}{col}]')
+        if g[r][col][0] == "#":
             neighbours += 1
             break
     # bottom
-    if row == testr and col == testc:
-        print(f'[{row}][{col}] Testing bottom')
-    for r in range(row+1,ROWLEN):
-        if g[r][col][0] == 'L':
+
+    for r in range(row + 1, ROWLEN):
+        if g[r][col][0] == "L":
             break
-        if g[r][col][0] == '#':
-            if row == testr and col == testc:
-                print(f'   occupied at [{r}{col}]')
+        if g[r][col][0] == "#":
             neighbours += 1
             break
     # left
-    if row == testr and col == testc:
-        print(f'[{row}][{col}] Testing left')
-    for c in range(col-1,-1,-1):
-        if g[row][c][0] == 'L':
+
+    for c in range(col - 1, -1, -1):
+        if g[row][c][0] == "L":
             break
-        if g[row][c][0] == '#':
-            if row == testr and col == testc:
-                print(f'   occupied at [{row}{c}]')
+        if g[row][c][0] == "#":
             neighbours += 1
             break
     # right
-    if row == testr and col == testc:
-        print(f'[{row}][{col}] Testing right')
-    for c in range(col+1,COLLEN):
-        if g[row][c][0] == 'L':
+    for c in range(col + 1, COLLEN):
+        if g[row][c][0] == "L":
             break
-        if g[row][c][0] == '#':
-            if row == testr and col == testc:
-                print(f'   occupied at [{row}{c}]')
+        if g[row][c][0] == "#":
             neighbours += 1
             break
     # topleft
-    if row == testr and col == testc:
-        print(f'[{row}][{col}] Testing topleft')
-    for r,c in zip(range(row-1,-1,-1),range(col-1,-1,-1)):
-        if g[r][c][0] == 'L':
+    for r, c in zip(range(row - 1, -1, -1), range(col - 1, -1, -1)):
+        if g[r][c][0] == "L":
             break
-        if g[r][c][0] == '#':
-            if row == testr and col == testc:
-                print(f'   occupied at [{r}{c}]')
+        if g[r][c][0] == "#":
             neighbours += 1
             break
     # topright
-    if row == testr and col == testc:
-        print(f'[{row}][{col}] Testing topright')
-    for r,c in zip(range(row-1,-1,-1), range(col+1,COLLEN)):
-        if g[r][c][0] == 'L':
+    for r, c in zip(range(row - 1, -1, -1), range(col + 1, COLLEN)):
+        if g[r][c][0] == "L":
             break
-        if g[r][c][0] == '#':
-            if row == testr and col == testc:
-                print(f'   occupied at [{r}{c}]')
+        if g[r][c][0] == "#":
             neighbours += 1
             break
     # bottomleft
-    if row == testr and col == testc:
-        print(f'[{row}][{col}] Testing bottom left')
-    for r,c in zip(range(row+1,ROWLEN),range(col-1,-1,-1)):
-        if g[r][c][0] == 'L':
+    for r, c in zip(range(row + 1, ROWLEN), range(col - 1, -1, -1)):
+        if g[r][c][0] == "L":
             break
-        if g[r][c][0] == '#':
-            if row == testr and col == testc:
-                print(f'   occupied at [{r}{c}]')
+        if g[r][c][0] == "#":
             neighbours += 1
             break
     # bottomright
-    if row == testr and col == testc:
-        print(f'[{row}][{col}] Testing bottom right')
-    for r,c in zip(range(row+1,ROWLEN),range(col+1,COLLEN)):
-        if g[r][c][0] == 'L':
+    for r, c in zip(range(row + 1, ROWLEN), range(col + 1, COLLEN)):
+        if g[r][c][0] == "L":
             break
-        if g[r][c][0] == '#':
-            if row == testr and col == testc:
-                print(f'   occupied at [{r}{c}]')
+        if g[r][c][0] == "#":
             neighbours += 1
             break
-
-    if row == testr and col == testc:
-        print(f'Score for [{row}][{col}] = {neighbours}')
 
     return neighbours
 
 
 def update(g):
-#    print("Starting update")
     for row in range(ROWLEN):
-#        print('-----------')
         for col in range(COLLEN):
-#            print(f'Testing [{row},{col}][{g[row][col][0]}]')
             seat = g[row][col][0]
             # floor is never occupied
-            if seat == '.':
+            if seat == ".":
                 g[row][col][1] == seat
                 continue
 
-            o = occupation(g,row,col)
-#            print(f'score for [{row},{col}] = {o}')
+            o = occupation(g, row, col)
             if o == 0:
-#                print(f'occupying [{row},{col}]')
-                g[row][col][1] = '#'
-            if o >= 5 and g[row][col][0] == '#':
-#                print(f'vacating [{row},{col}]')
-                g[row][col][1] = 'L'
+                g[row][col][1] = "#"
+            if o >= 5 and g[row][col][0] == "#":
+                g[row][col][1] = "L"
+
 
 def check(g):
     changes = 0
@@ -139,30 +106,66 @@ def check(g):
             if g[row][col][0] != g[row][col][1]:
                 changes = changes + 1
                 g[row][col][0] = g[row][col][1]
-    return g,changes
+    return changes
+
 
 def show(g):
     s = 0
     for row in range(ROWLEN):
         print("".join([s[0] for s in g[row]]))
-#        print("".join([s[0] for s in g[row]]), " ---> ", "".join([s[1] for s in g[row]]))
-        s += "".join([s[0] for s in g[row]]).count('#')
-    print(f'{s} occupied seats')
+        s += "".join([s[0] for s in g[row]]).count("#")
+    print(f"{s} occupied seats")
 
 
+
+
+
+def plot(row, col, colour):
+    offx = row * 10
+    offy = col * 10
+    points = [offx, offy, offx, offy + 10, offx + 10, offy + 10, offx + 10, offy]
+    w.create_polygon(points, outline="#cccccc", fill=colour)
+
+
+def show_tk(g):
+    for row in range(ROWLEN):
+        for col in range(COLLEN):
+            if g[row][col][0] == ".":
+                colour = "black"
+            if g[row][col][0] == "#":
+                colour = "red"
+            if g[row][col][0] == "L":
+                colour = "green"
+            plot(row, col, colour)
+
+
+canvas_width = 1000
+canvas_height = 1100
+tk = Tk()
+w = Canvas(tk, width=canvas_width, height=canvas_height)
+w.pack()
+
+#show(grid)
+show_tk(grid)
+tk.update_idletasks()
+tk.update()
+time.sleep(10)
+#tk.update_idletasks()
+#tk.update()
+#time.sleep(100)
+#sys.exit(1)
 runs = 1
 while True:
-    print("Run", runs)
     update(grid)
     runs = runs + 1
-    grid,changes = check(grid)
-    print(f'Changes = {changes}')
+    #show(grid)
+    show_tk(grid)
+    tk.update_idletasks()
+    tk.update()
+    changes = check(grid)
     if changes == 0:
         break
-
-
-show(grid)
-print(f'{runs} runs.')
-    
-
-
+show_tk(grid)
+#show(grid)
+print(f"{runs} runs.")
+time.sleep(100)
